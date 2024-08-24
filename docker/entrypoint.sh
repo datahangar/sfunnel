87ce2223f7a7dd737e9ed1b8e43d143f742a1b5b
Marc Sune <marcdevel@gmail.com>
2024-08-24 12:42:57 +0200

    docker: wait for interfaces to be ready
    At entrypoint time, kernel network interfaces might not be ready for
    the clsact qdisc to be attached. This manifests as:
    
    ```
    Error: Exclusivity flag on, cannot modify.
    ```
    
    The solution is to wait. This commit retries adding the tc qdisc
    up to N_TIMES=5, and adds a delay of 5s between executions.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,21 @@
 set -e
 set -x
 
+N_ATTEMPTS=5
+RETRY_DELAY=5
+PROG=tc_$1.o
+
 #Compile eBPF programs
 compile(){
 	cd /opt/sfunnel
 	make compile
+}
+
+#$1: PROG
+#$2: IFACE
+load_prog(){
+	tc qdisc add dev $2 clsact
+	tc filter add dev $2 ingress bpf da obj /opt/sfunnel/$1 sec funnel verbose
 }
 
 #$1: ACTION funnel/unfunnel
@@ -15,14 +26,22 @@ if [[ "$1" != "funnel" && "$1" != "unfunnel" ]]; then
 	exit 1;
 fi
 
-PROG=tc_$1.o
-
 #Compile for this specific kernel
 #compile
+
+#Show
 ls -la /opt/sfunnel
 
-#Attach
+#Load
 for IFACE in $(ls /sys/class/net); do
-	tc qdisc add dev $IFACE clsact
-	tc filter add dev $IFACE ingress bpf da obj /opt/sfunnel/$PROG sec funnel verbose
+	for ((i=1; i<=$N_ATTEMPTS; i++)); do
+		echo "Attaching BPF program '$PROG' to '$IFACE' using clsact qdisc..."
+		load_prog $PROG $IFACE && break
+		echo "WARNING: attempt $i failed on iface '$IFACE'. Retrying in $RETRY_DELAY seconds..."
+		sleep 5
+	done
+	if [[ $i -ge $N_ATTEMPTS ]]; then
+		echo "ERROR: unable to attach '$PROG' to '$IFACE'!"
+		exit 1
+	fi
 done
