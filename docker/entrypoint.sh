a9ab4042e9914c6d2cc722da5fcc64928af33a7c
Marc Sune <marcdevel@gmail.com>
2024-08-27 15:57:03 +0200

    docker/entrypoint.sh: improve traces
    Improve traces:
    
    * Remove -x
    * Move all traces from  NOTICE to INFO
    * Show a last success message with all the interfaces to which the
      BPF program has been attached.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-set -x
+#set -x
 
 N_ATTEMPTS=5
 RETRY_DELAY=5
@@ -25,38 +25,40 @@ load_prog(){
 
 #If SFUNNEL_RULESET is defined, create the file
 if [[ "$SFUNNEL_RULESET" != "" ]]; then
-	echo "[NOTICE] SFUNNEL_RULESET='$SFUNNEL_RULESET'"
+	echo "[INFO] SFUNNEL_RULESET='$SFUNNEL_RULESET'"
 	echo $SFUNNEL_RULESET > /opt/sfunnel/src/ruleset
 fi
 
 #Compile sfunnel only if new ruleset is specified
 if test -f /opt/sfunnel/src/ruleset; then
-	echo "[NOTICE] Compiling sfunnel with custom ruleset..."
+	echo "[INFO] Compiling sfunnel with custom ruleset..."
 	echo "==="
 	cat /opt/sfunnel/src/ruleset
 	echo "==="
 	compile
 else
-	echo "[NOTICE] Using default ruleset..."
+	echo "[INFO] Using default ruleset..."
 	echo "==="
 	cat /opt/sfunnel/src/ruleset.default
 	echo "==="
 fi
 
-#Show
-ls /opt/sfunnel
-ls /opt/sfunnel/src
-
 #Load
-for IFACE in $(ls /sys/class/net); do
+IFACES=$(ls /sys/class/net | tr "\n" " " | sed 's/\s*$//g')
+
+echo ""
+echo -e "[INFO] Attaching BPF program '$PROG' to IFACES={$IFACES} using clsact qdisc...\n"
+for IFACE in $IFACES; do
 	for ((i=1; i<=$N_ATTEMPTS; i++)); do
-		echo "Attaching BPF program '$PROG' to '$IFACE' using clsact qdisc..."
+		echo "[INFO] Attaching BPF program to '$IFACE'..."
 		load_prog $PROG $IFACE && break
-		echo "WARNING: attempt $i failed on iface '$IFACE'. Retrying in $RETRY_DELAY seconds..."
+		echo "[WARNING] attempt $i failed on iface '$IFACE'. Retrying in $RETRY_DELAY seconds..."
 		sleep 5
 	done
 	if [[ $i -ge $N_ATTEMPTS ]]; then
-		echo "ERROR: unable to attach '$PROG' to '$IFACE'!"
+		echo "[ERROR] unable to attach BPF program to '$IFACE'!"
 		exit 1
 	fi
+	echo -e "[INFO] Successfully attached BPF program to '$IFACE'.\n"
 done
+echo "[INFO] Successfully attached '$PROG' to interfaces {$IFACES}"
