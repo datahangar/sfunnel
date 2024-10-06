2ffa97c1498e279fc3434abce5a683a37a640257
Marc Sune <marcdevel@gmail.com>
2024-10-06 14:19:08 +0200

    test: run affinity test on multiple IPs
    This commit modifies Makefile and check_affinity scripts to optionally
    run checks from a list of SRC_IPs.
    
    It also renames TIMER to ITERATIONS to make more obvious the
    check_affinity script behaviour. Added some usefule traces too.

--- test/cni/check_affinity.sh
+++ test/cni/check_affinity.sh
@@ -3,27 +3,31 @@
 #
 # Check affinity between port 80 and 8080
 #
-# $1: test time in seconds
-#
+# Env:
+# $ITERATIONS: number of iterations (~1sec per iteration)
+# $SRC_IPS: check affinity from (e.g. "192.168.254.2 192.168.254.3")
+
+if [[ "${DEBUG}" != "" ]]; then
+	set -x
+fi
+
+ITERATIONS=${ITERATIONS:-1}
+LB_IP=${LB_IP:-$(minikube kubectl -- get service my-loadbalancer-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')}
 
 #$1: URL
+#$2: SRC_IP
 get_pod(){
-	sudo ip netns exec client curl -s ${1} | sed 's/^.*by\s*//'
+	CURL_OPTS=""
+	if [[ "${2}" != "" ]]; then
+		CURL_OPTS=" --interface ${2}"
+	fi
+	sudo ip netns exec client curl ${CURL_OPTS} -s ${1} | sed 's/^.*by\s*//'
 }
 
-
-LB_IP="$(minikube kubectl -- get service my-loadbalancer-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
-
-#Wait for nginx to be operative
-while $( [[ "$(get_pod ${LB_IP}:80 | grep my-nginx-deployment)" == "" ]] ); do
-	sleep 1
-
-done
-
-i=0
-while true; do
-	HTTP=$(get_pod ${LB_IP}:80)
-	ALT_HTTP=$(get_pod ${LB_IP}:8080)
+#$1: SRC_IP or ""
+check_affinity(){
+	HTTP=$(get_pod ${LB_IP}:80 ${1})
+	ALT_HTTP=$(get_pod ${LB_IP}:8080 ${1})
 	if [[ "${HTTP}" != "${ALT_HTTP}" ]]; then
 		echo "ERROR: affinity NOT respected"
 		echo "HTTP: ${HTTP}"
@@ -31,12 +35,36 @@ while true; do
 		exit 1
 	fi
 
-	((i++))
-	if [[ $i -eq ${1} ]]; then
-		break
+	echo "Serving pod: "
+	echo "  HTTP:     ${HTTP}"
+	echo "  ALT_HTTP: ${ALT_HTTP}"
+}
+
+#Wait for nginx to be operative
+while $( [[ "$(get_pod ${LB_IP}:80 | grep my-nginx-deployment)" == "" ]] ); do
+	sleep 1
+
+done
+
+i=1
+while true; do
+	echo "Iteration ${i}/${ITERATIONS}..."
+	if [[ "${SRC_IPS}" == "" ]]; then
+		check_affinity
+	else
+		for IP in ${SRC_IPS}; do
+			echo "Checking port affinity to ${LB_IP} from ${IP}..."
+			check_affinity ${IP}
+		done
 	fi
 
+	if [[ $i -eq ${ITERATIONS} ]]; then
+		break
+	fi
+	((i++))
+
 	sleep 1;
+	echo ""
 done
 
 echo "OK"
