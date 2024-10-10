744c7bf9a4a4674293d6353651f6e8c326544e56
Marc Sune <marcdevel@gmail.com>
2024-10-10 20:00:26 +0200

    Tc

--- test/cni/perf/gen_report.py
+++ test/cni/perf/gen_report.py
@@ -1,3 +1,24 @@
 import re
+import json
+import platform
 
+with open('.last_perf_report.json', 'r') as file:
+    data = json.load(file)
 
+md_report = "# Test reports\n"
+md_report += f"Architecture: {platform.machine()}\n"
+md_report += f"Kernel: {platform.release()}\n"
+md_report += f"OS: {platform.system()}\n"
+md_report += f"Hostname: {platform.node()}\n"
+md_report += f"\n"
+
+for key, elem in data.items():
+    md_report += f"## Test: {key}\n"
+    md_report += f"\n"
+    md_report += f"Number of workers: {elem['number_of_workers']}\n"
+    md_report += f"\n"
+    md_report += f"Average throughput per worker: {elem['average_throughput']:.2f} Mbit/s\n"
+    md_report += f"Total throughput: {elem['total_throughput']:.2f} Mbit/s\n"
+    md_report += f"\n"
+
+print(md_report)
